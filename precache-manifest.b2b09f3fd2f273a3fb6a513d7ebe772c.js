5a7147f1217c46059ce4b54321c29d4160b2534c
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-04-22 10:35:27 -0500

    Updates

---
+++ precache-manifest.b2b09f3fd2f273a3fb6a513d7ebe772c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "76722f1d434905c6bbd5",
+    "url": "/about-me-test/static/css/main.465e6302.chunk.css"
+  },
+  {
+    "revision": "76722f1d434905c6bbd5",
+    "url": "/about-me-test/static/js/main.76722f1d.chunk.js"
+  },
+  {
+    "revision": "1e759b79f5ea69d4f0fd",
+    "url": "/about-me-test/static/js/runtime~main.1e759b79.js"
+  },
+  {
+    "revision": "6ea6c0c53a8d1c8c3ecc",
+    "url": "/about-me-test/static/css/2.9fd9c8c3.chunk.css"
+  },
+  {
+    "revision": "6ea6c0c53a8d1c8c3ecc",
+    "url": "/about-me-test/static/js/2.6ea6c0c5.chunk.js"
+  },
+  {
+    "revision": "f6e54d53710dcf3b62e35925800f0d9c",
+    "url": "/about-me-test/static/media/desarrollo-web.f6e54d53.png"
+  },
+  {
+    "revision": "c3ba82a536fe4983dad4c06b578dcd5d",
+    "url": "/about-me-test/static/media/react.c3ba82a5.png"
+  },
+  {
+    "revision": "8bfc5015da73628edae2996b01ed50c0",
+    "url": "/about-me-test/static/media/laravel.8bfc5015.png"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/about-me-test/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "5b68c4dc85a9d0f7c57e3475b6f7c98f",
+    "url": "/about-me-test/static/media/idea.5b68c4dc.svg"
+  },
+  {
+    "revision": "ea92d6580dda0fbcf61a8ccb16e0d02a",
+    "url": "/about-me-test/static/media/postgresql.ea92d658.png"
+  },
+  {
+    "revision": "07c28adf28e0592b46094aef81fcc655",
+    "url": "/about-me-test/static/media/mysql.07c28adf.png"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/about-me-test/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "6061adf9daddb72f5f905e4cad270f3a",
+    "url": "/about-me-test/index.html"
+  }
+];
