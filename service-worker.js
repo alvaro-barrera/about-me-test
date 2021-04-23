032db835c2b90ab347c77d16ccd11f2c7fef6691
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-04-23 13:26:09 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/about-me-test/precache-manifest.7856d1e9059b8d7d7cde45c54d95d703.js"
+  "/about-me-test/precache-manifest.287c5857aa26e544e52a1b62248ed8b4.js"
 );
 
 workbox.clientsClaim();
